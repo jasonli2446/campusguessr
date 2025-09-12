248e95c7e9fce1537bc258b027a7ebf9168517fb
Jason Li <jasonli2446@gmail.com>
2025-09-12 19:46:04 -0400

    dont look at this commit

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,8 @@
 import type { NextConfig } from "next";
 
+console.log("SUPABASE URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
+console.log("SUPABASE PUBLISHABLE KEY:", process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY);
+
 const nextConfig: NextConfig = {
   /* config options here */
 };
